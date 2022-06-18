a6d16a38c6d84853af6f971bbf6d409485ea8a41
Arya <aryamishra.in@gmail.com>
2022-06-19 00:12:13 +0100

    ai bot

--- app.py
+++ app.py
@@ -1,13 +1,18 @@
 import json
 from flask import Flask, request, jsonify
+import chat
+
+print("In module products __package__, __name__ ==", __package__, __name__)
 
 app = Flask(__name__, static_url_path='/static')
 
 @app.route('/chat', methods=['POST'])
-def chat():
+def talk():
     print(request.json)
     record = request.json
-    return jsonify({'message': record["message"]})
+    response = chat.say(record["message"])
+    print("response ", response)
+    return jsonify({'message': response})
 
 app.run(debug=True)
 
